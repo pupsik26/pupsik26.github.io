46bed9935e5106791b619448540c9a6401b446a1
pupsik26 <56516091+pupsik26@users.noreply.github.com>
2021-08-03 03:06:26 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -34,4 +34,21 @@ function onEntry(entry) {
     observer.observe(elm);
   }
  
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu'),
+  menuItem = document.querySelectorAll('.menu_item'),
+  hamburger = document.querySelector('.menu_mobile');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('menu_mobile_active');
+      menu.classList.toggle('menu_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('menu_mobile_active');
+          menu.classList.toggle('menu_active');
+      })
+  })
+})
 
